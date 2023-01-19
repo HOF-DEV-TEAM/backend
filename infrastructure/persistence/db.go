469fbab85ce0a0700ac3c38ee8d6de5a15d12bee
waire <waire.tega@gmail.com>
2023-01-19 16:06:50 +0100

    HOFB-0002|Fix: fixed breaking changes

--- infrastructure/persistence/db.go
+++ infrastructure/persistence/db.go
@@ -3,8 +3,6 @@ package persistence
 import (
 	"bitbucket.org/hofng/hofApp/domain/repository"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readconcern"
 	"go.uber.org/zap"
 )
 
@@ -19,5 +17,5 @@ func New(client *mongo.Client, databaseName string, logger *zap.Logger) reposito
 }
 
 func (repo *mongoStore) col(collectionName string) *mongo.Collection {
-	return repo.mongoClient.Database(repo.databaseName, &options.DatabaseOptions{ReadConcern: readconcern.Local()}).Collection(collectionName)
+	return repo.mongoClient.Database(repo.databaseName).Collection(collectionName)
 }
