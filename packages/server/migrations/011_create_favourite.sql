e4314f6a0d55736e690568dd47e079f4b8a8439c
waire <waire.tega@gmail.com>
2023-03-12 15:17:43 +0100

    MHA-70|Refactor: add delete cascade to table

--- packages/server/migrations/011_create_favourite.sql
+++ packages/server/migrations/011_create_favourite.sql
@@ -2,7 +2,7 @@ create table if not exists favourites(
     "id" uuid not null default gen_random_uuid() primary key,
     "user_id" uuid not null,
     "fav" JSONB,
-    constraint "Fk_users" foreign key ("user_id") references "users" ("id")
+    constraint "Fk_users" foreign key ("user_id") references "users" ("id") ON DELETE CASCADE
     );
 
 ---- create above / drop below ----
